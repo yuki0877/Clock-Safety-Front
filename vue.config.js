60ed59b51830afc7eae54e74a33a23d2ba630d95
yuki0877 <yuki.lucky.0927@gmail.com>
2024-08-31 22:32:59 +0900

    publicPathを追加

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  assetsDir: './',
-  // publicPath: './',
+  // assetsDir: './',
+  publicPath: './',
   devServer: {
     proxy: {
       "/api/v1": {
