89dd4c96c2056893230ba2257adee697a182891d
yuki0877 <yuki.lucky.0927@gmail.com>
2024-08-31 22:40:08 +0900

    publicPathを変更

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  // assetsDir: './',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Clock-Safety-Front' : '/',
   devServer: {
     proxy: {
       "/api/v1": {
