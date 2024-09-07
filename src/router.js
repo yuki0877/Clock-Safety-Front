8b5a31c5c9ef23f54608915dc21b9e5d7cce9859
yuki0877 <yuki.lucky.0927@gmail.com>
2024-09-08 04:36:45 +0900

    router更新

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ const routes = [
   // },
 ]
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/Clock-Safety-Front/' : '/'),
+  history: createWebHistory('/'),
   routes,
 })
 
