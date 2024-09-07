513f650d1bc51ff191e3c7f434acfa13da0157e1
yuki0877 <yuki.lucky.0927@gmail.com>
2024-09-08 04:31:37 +0900

    configを変更

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'docs',
+  outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production' ? '/Clock-Safety-Front/' : '/',
   devServer: {
     proxy: {
-      "/api/v1": {
+      '/api/v1': {
         target: process.env.VUE_APP_API_BASE_URL,
         changeOrigin: true,
         pathRewrite: {
-          '^/api/v1': ''
-        }
-      }
-    }
+          '^/api/v1': '',
+        },
+      },
+    },
   },
   configureWebpack: {
     plugins: [
-      new (require('webpack')).DefinePlugin({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
-      })
-    ]
-  }
+      new (require('webpack').DefinePlugin)({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+    ],
+  },
 })
