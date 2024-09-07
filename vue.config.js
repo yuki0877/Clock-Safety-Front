cd8491997438f36409cf5e33a2addb0012507f17
yuki0877 <yuki.lucky.0927@gmail.com>
2024-09-08 04:34:30 +0900

    publicパス変更

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/Clock-Safety-Front/' : '/',
+  publicPath: '/',
   devServer: {
     proxy: {
       '/api/v1': {
